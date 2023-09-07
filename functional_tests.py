58e98bb3057547392ec6c47a15fa7e145b97b83c
Bruno <brunodavidmoreirabh2@gmail.com>
2023-09-07 13:03:17 -0300

    From page html now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -35,6 +35,11 @@ class NewVisitorTest(unittest.TestCase):
 
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New To-do item did not apper in table"
         )
         self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
