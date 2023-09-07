dd6202a1d84567396d4556fc328a3ff27adf4841
Bruno <brunodavidmoreirabh2@gmail.com>
2023-09-07 12:48:12 -0300

    Refactor home_page views to use a templetes

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,19 +1,9 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-from lists.views import home_page
-from django.template.loader import render_to_string
 
 
 class HomePageTest(TestCase):
 
-    def test_home_page_returns_corrent_html(self):
+    def test_uses_home_templates(self):
 
         response = self.client.get('/')
-
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-
         self.assertTemplateUsed(response, 'home.html')
