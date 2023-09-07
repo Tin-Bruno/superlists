d5fa1641e172045080f8113f4205aaaa5576125e
Bruno <brunodavidmoreirabh2@gmail.com>
2023-09-07 14:57:40 -0300

    atualização simples

--- functional_tests.py
+++ functional_tests.py
@@ -29,15 +29,11 @@ class NewVisitorTest(unittest.TestCase):
         )
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-
         time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
 
+        table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New To-do item did not apper in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         self.fail('Finish the test!')
 
 
